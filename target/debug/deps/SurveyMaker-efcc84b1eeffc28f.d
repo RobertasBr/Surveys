adbdf4a1d80f66f8cf1ee69f5526447a92975490
RobertasBr <123648407+RobertasBr@users.noreply.github.com>
2024-10-15 20:37:52 +0100

    first commit

---
+++ target/debug/deps/SurveyMaker-efcc84b1eeffc28f.d
@@ -0,0 +1,5 @@
+H:\Project300\SurveyMaker\target\debug\deps\libSurveyMaker-efcc84b1eeffc28f.rmeta: src/main.rs
+
+H:\Project300\SurveyMaker\target\debug\deps\SurveyMaker-efcc84b1eeffc28f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/SurveyMaker-f4b318b8279b7115.d
@@ -0,0 +1,5 @@
+H:\Project300\SurveyMaker\target\debug\deps\libSurveyMaker-f4b318b8279b7115.rmeta: src/main.rs
+
+H:\Project300\SurveyMaker\target\debug\deps\SurveyMaker-f4b318b8279b7115.d: src/main.rs
+
+src/main.rs:
